0fb08ae7f4f253ee4fc81017b1fcf580a8bdf6bd
skosachiov <skosachiov@gmail.com>
2022-11-20 19:46:34 +0300

    fix unittest

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class TestHFTGenerator(unittest.TestCase):
 
     def test_single_set_generation(self):
         d = hft_producer.hft_dataset(100)
-        self.assertTrue(1 <= d['prices']['values'][9] and d['prices']['values'][9] <= 100)
+        self.assertTrue(1 <= d['prices_values'][9] and d['prices_values'][9] <= 100)
 
     def test_series_of_datasets(self):
         r = hft_producer.hft_datastream(100, 0.001, 10)
